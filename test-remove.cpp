ee3180ab00e39d24ba8d1f17bd3ecbd6cea03872
PretzelHH <90863865+PretzelHH@users.noreply.github.com>
2023-03-23 14:43:03 -0700

    Update test remove

--- test-remove.cpp
+++ test-remove.cpp
@@ -1,14 +1,3 @@
-// // I will test your BinTree class using either this main or
-// // a very similar one, and this buildTree.
-// // You will want to do thorough testing on your own,
-// // which is done by altering the data.
-
-// // Windows and unix store files slightly differently. Windows does not
-// // always store an end-of-line char on the last line, where unix does.
-// // On windows/mac, to mimic the way linux stores files, always make sure
-// // the cursor is on the line after the last line of data.
-
-// // presumably bintree.h includes nodedata.h so the include is not needed here
 // #include "bintree.h"
 // #include <fstream>
 // #include <iostream>
